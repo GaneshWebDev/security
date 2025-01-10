d8acc3e638875a77dda5f5d299b7ca49be8d9666
20215ganesh <muzinoasahi@gmail.com>
2025-01-10 17:45:47 +0530

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express=require('express');
 const app=express();
+app.use('/',(req,res)=>{
+    res.json({message:'hello world'})
+})
 app.listen(5000,()=>{
     console.log('server lsitening to port number 5000')
 })
